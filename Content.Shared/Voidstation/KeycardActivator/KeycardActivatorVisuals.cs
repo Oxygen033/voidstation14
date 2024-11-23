b6e3fb79900aa57b220c05fae26c843ee3cdc2e4
Oxygen033 <moormaxim2k4@gmail.com>
2024-11-23 09:00:27 +0600

    keycard activator 1

---
+++ Content.Shared/Voidstation/KeycardActivator/KeycardActivatorVisuals.cs
@@ -0,0 +1,10 @@
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.VoidStation.KeycardActivator
+{
+    [Serializable, NetSerializable]
+    public enum KeycardActivatorVisuals
+    {
+        ActivationState
+    }
+}
